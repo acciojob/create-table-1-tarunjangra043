7e7f25c3977ac98f18e9dd72a9009a96141f61ee
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-07-22 08:53:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  let table = document.getElementById('sampleTable');
+
+	let row = table.insertRow(0);
+
+	let cell1 = row.insertCell(0);
+	let cell2 = row.insertCell(1);
+
+	cell1.innerHTML = 'New Cell1';
+	cell2.innerHTML = 'New Cell2';
 }
